4898d13fb9aebfe2585784f9d2069f7ececc2684
Parth Gajjar <parthegajjar@gmail.com>
2020-03-05 19:51:00 +0530

    [IMP] files for chapter 06

---
+++ Chapter06/11_extend_business_logic/my_library_return/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library Returns Dates",  # Module title
+    'summary': "Manage return dates for books",  # Module subtitle phrase
+    'author': "Parth Gajjar",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['my_library'],
+    'data': [
+        'views/library_book.xml',
+    ],
+    # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
