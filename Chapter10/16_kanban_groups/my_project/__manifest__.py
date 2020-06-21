7eab5f6990b388172ec92ae68a72cdf84e22b82a
Parth Gajjar <parthegajjar@gmail.com>
2020-06-22 02:27:50 +0530

    [IMP] added files for chapter 10

---
+++ Chapter10/16_kanban_groups/my_project/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Project",  # Module title
+    'description': """Long description""",  # You can also rst format
+    'author': "Parth Gajjar",
+    'website': "http://www.example.com",
+    'category': 'Project',
+    'version': '13.0.1',
+    'depends': ['project'],
+    'data': [
+        'views/my_tasks.xml',
+    ],
+}
