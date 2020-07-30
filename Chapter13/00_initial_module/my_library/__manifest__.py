4df6f5871f1cf987f606036d496fa2f87490fe9d
Parth Gajjar <parthegajjar@gmail.com>
2020-07-31 01:40:16 +0530

    added code file for chapter 13

---
+++ Chapter13/00_initial_module/my_library/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library",  # Module title
+    'summary': "Manage books easily",  # Module subtitle phrase
+    'description': """
+Manage Library
+==============
+Description related to library.
+    """,  # Supports reStructuredText(RST) format
+    'author': "Parth Gajjar",
+    'website': "http://www.example.com",
+    'category': 'Library',
+    'version': '13.0.1',
+    'depends': ['base'],
+
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/library_book_categ.xml',
+        'views/library_book_rent.xml',
+    ],
+
+    # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
+
+
+
+
